c74aa27cd33bfcbade021ddc60d7d110092bb8b7
Lorhana Trindade <lortrindade@gmail.com>
2018-03-27 00:07:55 -0300

    :rocket: first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+new Vue ({
+    el: '#vue',
+
+    data: {
+        cep: '',
+        endereco: {},
+    },
+
+    methods: {
+        buscar: function() {
+
+            var self = this;
+            if (/^[0-9]{5}-[0-9]{3}$/.test(this.cep)) {
+                jQuery.getJSON('http://viacep.com.br/ws/'+this.cep+'/json', function(endereco) {
+
+                    self.endereco = endereco;
+                    self.$refs.numero.focus();
+                });
+            }
+        }
+    }
+});
